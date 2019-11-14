7c7dcc39701994e98d386c9fcfc9cbbecc020bf5
Firuza Raiymkul <FRaiymkul@dar.lcl>
2019-11-14 18:14:09 +0600

    fix(): day

--- First/valtDate/app.js
+++ First/valtDate/app.js
@@ -1,38 +1,19 @@
-let movieYear = {
-	'John Wick' :'2014',
-	'Harry Potter' : '2011',
-	'The Matrix' : '2000',
-}
+
 
 let btn = document.getElementById('btn');
-document.write('<br> John Wick <br> The Matrix <br> Harry Potter')
 
 btn.onclick = function() {
-	let movie = prompt("Какой фильм вас интересует?");
+	let inputValue = document.getElementById('dateInput').value;
+	let date = new Date(inputValue);
 
-	for(let key in movieYear){
-		if(key.includes(movie)){
-		  alert(`фильм вышел в ${movieYear[key]} году`);
-		} else {
-			alert(`такого фильма нет в списке.`)
+	let day = date.getDay();
+	console.log('day', day)
+	array = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+
+	for (let i=0; i<array.length; i++) {
+		if(day === i) {
+			alert(array[i]);
+			console.log('data')
 		}
 	}
 }
-
-
-// btn.onclick = function() {
-// 	let request = fetch('http://www.omdbapi.com/?apikey=bbfde27a&t=John+Wick');
-
-// 	request.then(function(response) {
-// 		const jsonResp = response.json();
-// 		jsonResp.then(function(data) {
-// 			let movie = prompt("Choose movie")
-// 			if(movie == data.Title){
-// 				alert(data.Year);
-// 			} else {
-// 				alert("there is not such movie")
-// 			}
-// 			console.log(data);
-// 		})
-// 	})
-// }
